84bf2fe9bca38b8fe2185c3a6b8862f9a191c586
Andrew <thesimpledev@gmail.com>
2019-02-15 13:37:06 -0800

    Setup webpack, npm and react

---
+++ client/app.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+document.addEventListener("DOMContentLoaded", () => {
+  ReactDOM.render(<h1>Do the things</h1>, document.getElementById("root"));
+});
