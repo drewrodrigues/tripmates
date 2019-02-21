3f1566fcbb4afd5cdb75e740f7a40c91b0d07b62
Andrew <thesimpledev@gmail.com>
2019-02-20 21:49:05 -0800

    Set routes default to json

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   root to: "static_pages#root"
-  namespace :api do
-    resources :users, only: [:create, :destroy, :update]
+  namespace :api, defaults: {format: :json} do
+    resources :users, only: [:create, :destroy, :update] do
+      resources :trips, only: [:index, :create]
+    end
     resource  :session, only: [:create, :destroy]
+    resources :trips, except: [:index, :create, :new, :edit]
   end
 end
