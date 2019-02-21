da477be130d3ce5966b65a941a08b5383aae4ca3
Andrew <thesimpledev@gmail.com>
2019-02-20 21:48:34 -0800

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,4 +12,16 @@ ActiveRecord::Base.transaction do
                          location: "Supai, Arizona",
                        creator_id: drew.id,
                             title: "Camping in Supai")
+
+  new_york = Trip.create!(start_date: Date.today+10, 
+                            end_date: Date.today+20,
+                            location: "New York, New York",
+                          creator_id: drew.id,
+                               title: "Cross Country Train Trip")
+
+  new_york = Trip.create!(start_date: Date.today+300, 
+                            end_date: Date.today+305,
+                            location: "Lake Tahoe, California",
+                          creator_id: drew.id,
+                               title: "Firewatch Tower")
 end
