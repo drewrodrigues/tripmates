68e97542e2b5d31568b8c63f4b34ab0406c78d4a
Drew <thesimpledev@gmail.com>
2019-05-13 11:18:38 -0700

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,33 +4,196 @@ User.destroy_all
 Trip.destroy_all
 
 ActiveRecord::Base.transaction do
-  drew = User.create!(
+  SEED_DATA_PATH = "app/assets/seed_data/"
+
+  drew = User.new(
     first_name: "Drew",
     last_name: "Rodrigues",
     email: "thesimpledev@gmail.com",
     password: "password"
   )
+  drew.profile_picture.attach(
+    io: File.open(SEED_DATA_PATH + "users/" + "me.jpeg"),
+    filename: "me"
+  )
+  drew.save!
 
-  10.times do
-    User.create!(
-      first_name: Faker::Name.first_name,
-      last_name: Faker::Name.last_name,
+  users = [
+    {
+      first_name: "Aaron",
+      last_name: "Waugh",
+      profile_picture: SEED_DATA_PATH + "users/" + "aaron.jpg"
+    },
+    {
+      first_name: "Alisha",
+      last_name: "Rodrigues",
+      profile_picture: SEED_DATA_PATH + "users/" + "alisha.jpg"
+    },
+    {
+      first_name: "Billie",
+      last_name: "Zoller",
+      profile_picture: SEED_DATA_PATH + "users/" + "billie.jpg"
+    },
+    {
+      first_name: "David",
+      last_name: "Larson",
+      profile_picture: SEED_DATA_PATH + "users/" + "david.jpg"
+    },
+    {
+      first_name: "Desiree",
+      last_name: "Rodrigues",
+      profile_picture: SEED_DATA_PATH + "users/" + "desiree.jpg"
+    },
+    {
+      first_name: "Justin",
+      last_name: "Perry",
+      profile_picture: SEED_DATA_PATH + "users/" + "justin.jpg"
+    },
+    {
+      first_name: "Ken",
+      last_name: "O'Campo",
+      profile_picture: SEED_DATA_PATH + "users/" + "ken.jpg"
+    },
+    {
+      first_name: "Nathalie",
+      last_name: "Lopez",
+      profile_picture: SEED_DATA_PATH + "users/" + "nathalie.jpg"
+    },
+    {
+      first_name: "Nikkol",
+      last_name: "Vile",
+      profile_picture: SEED_DATA_PATH + "users/" + "nikkol.jpg"
+    },
+    {
+      first_name: "Temesgen",
+      last_name: "Fekadu",
+      profile_picture: SEED_DATA_PATH + "users/" + "tdog.jpg"
+    }
+  ]
+
+  users.each do |user|
+    new_user = User.new(
+      first_name: user[:first_name],
+      last_name: user[:last_name],
       email: Faker::Internet.email,
       password: "password"
     )
+    new_user.profile_picture.attach(
+      io: File.open(user[:profile_picture]),
+      filename: user[:profile_picture]
+    )
+    new_user.save!
   end
 
   user_ids = User.ids
 
-  200.times do |i|
+  trips = [
+    {
+      cover_photo: SEED_DATA_PATH + "trips/" + "backpacking.jpg",
+      title: "Backpacking",
+      location: "Europe"
+    },
+    {
+      cover_photo: SEED_DATA_PATH + "trips/" + "beach_day.jpg",
+      title: "Beach Day",
+      location: "Santa Cruz, California"
+    },
+    {
+      cover_photo: SEED_DATA_PATH + "trips/" + "camping.jpeg",
+      title: "Camping",
+      location: "Camping"
+    },
+    {
+      cover_photo: SEED_DATA_PATH + "trips/" + "camping.jpeg",
+      title: "Camping",
+      location: "Camping"
+    },
+    {
+      cover_photo: SEED_DATA_PATH + "trips/" + "clubbing.jpeg",
+      title: "Clubbing",
+      location: "Miami, Florida"
+    },
+    {
+      cover_photo: SEED_DATA_PATH + "trips/" + "cross_country_biking.jpg",
+      title: "Cross Country Ride",
+      location: "United States"
+    },
+    {
+      cover_photo: SEED_DATA_PATH + "trips/" + "cruise.jpg",
+      title: "Cruise",
+      location: "Mexico"
+    },
+    {
+      cover_photo: SEED_DATA_PATH + "trips/" + "hike.jpg",
+      title: "Hike",
+      location: "Yosemite, California"
+    },
+    {
+      cover_photo: SEED_DATA_PATH + "trips/" + "peru.jpg",
+      title: "Machu Picchu",
+      location: "Peru"
+    },
+    {
+      cover_photo: SEED_DATA_PATH + "trips/" + "rafting.jpg",
+      title: "White Water Rafting",
+      location: "Sacremento, California"
+    },
+    {
+      cover_photo: SEED_DATA_PATH + "trips/" + "rails_conf.png",
+      title: "Rails Conference",
+      location: "Pheonix, Arizona"
+    },
+    {
+      cover_photo: SEED_DATA_PATH + "trips/" + "roadtrip_2.jpg",
+      title: "Road Trip",
+      location: "Portland, Oregon"
+    },
+    {
+      cover_photo: SEED_DATA_PATH + "trips/" + "roadtrip.jpg",
+      title: "Road Trip",
+      location: "Austin, Texas"
+    },
+    {
+      cover_photo: SEED_DATA_PATH + "trips/" + "sky_diving.jpg",
+      title: "Sky Diving",
+      location: "Davis, California"
+    },
+    {
+      cover_photo: SEED_DATA_PATH + "trips/" + "snowboarding.jpg",
+      title: "Snowboarding",
+      location: "Lake Tahoe, California"
+    },
+    {
+      cover_photo: SEED_DATA_PATH + "trips/" + "tahoe.jpg",
+      title: "Weekend in Tahoe",
+      location: "Lake Tahoe, California"
+    },
+    {
+      cover_photo: SEED_DATA_PATH + "trips/" + "train.jpg",
+      title: "Train Trip",
+      location: "Chicago, California"
+    },
+    {
+      cover_photo: SEED_DATA_PATH + "trips/" + "wilderness.jpg",
+      title: "Wilderness Backpacking",
+      location: "Yosemite, California"
+    }
+  ]
+
+  trips.each do |trip|
     start_date = Faker::Time.between(20.days.from_now, 2.years.from_now)
-    supai = Trip.create!(
+    new_trip = Trip.new(
       start_date: start_date,
       end_date: Faker::Time.between(start_date, start_date + 10.days),
-      location: Faker::Nation.capital_city,
+      location: trip[:location],
       creator_id: user_ids.sample,
-      title: Faker::Marketing.buzzwords.titleize,
-      image_url: "https://picsum.photos/1600/900?image=#{i + 10}"
+      title: trip[:title],
+      spaces: (2..10).to_a.sample
     )
+    new_trip.cover_photo.attach(
+      io: File.open(trip[:cover_photo]),
+      filename: trip[:cover_photo]
+    )
+    new_trip.save!
   end
 end
