7eb03df45261f9724dcc540667c64665fbd55772
Drew <thesimpledev@gmail.com>
2019-05-13 17:28:27 -0700

    Pull current user into selector

--- client/components/Shared/navContainer.jsx
+++ client/components/Shared/navContainer.jsx
@@ -1,11 +1,14 @@
-import { connect } from 'react-redux';
-import Nav from './nav';
-import { logout } from '../../actions/sessionActions';
-import { withRouter } from 'react-router-dom';
+import { connect } from 'react-redux'
+import Nav from './nav'
+import { logout } from '../../actions/sessionActions'
+import { withRouter } from 'react-router-dom'
+import { currentUser } from '../../selectors/sessionSelectors'
 
-const mapStateToProps = state => ({
-  currentUser: state.session.id
-});
+const mapStateToProps = state => {
+  return {
+    currentUser: currentUser(state)
+  }
+}
 
 const mapDispatchToProps = dispatch => ({
   logout: () => dispatch(logout())
