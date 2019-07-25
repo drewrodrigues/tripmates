3076e319f95f201016e4fa0c23cb78b22714f357
Drew <thesimpledev@gmail.com>
2019-07-25 11:16:43 -0700

    Make sign up link look nice

--- client/components/Shared/navLoggedOut.jsx
+++ client/components/Shared/navLoggedOut.jsx
@@ -12,7 +12,7 @@ const NavLoggedOut = () => (
         <NavLink className="Nav-link" to="/home">Home</NavLink>
         <NavLink className="Nav-link" to="/about">About</NavLink>
         <NavLink className="Nav-link" to="/login">Login</NavLink>
-        <NavLink className="Nav-link" to="/signup">Sign Up</NavLink>
+        <NavLink className="Nav-link Nav-link-sign-up" to="/signup">Sign Up</NavLink>
       </nav>
     </div>
   </div>
