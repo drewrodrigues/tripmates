8102617bee9f878cf4bb183d668f903e8e8b867b
Drew <thesimpledev@gmail.com>
2019-08-05 23:20:26 -0700

    Fix trip routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api, defaults: {format: :json} do
     resources :users, only: [:create, :destroy, :update, :index]
-    resources :trips, only: :index
+    resources :trips, except: [:new, :edit]
     resources :friend_requests, only: [:create, :index, :destroy]
     resource  :session, only: [:create, :destroy]
     resources :trips, except: [:index, :new, :edit]
