fff679682c33c4d38bde9312eac33984d593c42d
Drew Rodrigues <thesimpledev@gmail.com>
2019-08-08 13:37:48 -0700

    Add friendsReducer

--- client/src/reducers/entitiesReducer.js
+++ client/src/reducers/entitiesReducer.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux'
 import tripsReducer from './tripsReducer'
 import usersReducer from './usersReducer'
 import friendRequestsReducer from './friendRequestsReducer'
+import friendsReducer from "./friendsReducer"
 
 export default combineReducers({
+  friendRequests: friendRequestsReducer,
+  friends: friendsReducer,
   trips: tripsReducer,
-  users: usersReducer,
-  friendRequests: friendRequestsReducer
+  users: usersReducer
 })
---
+++ client/src/reducers/friendsReducer.js
@@ -0,0 +1,24 @@
+import {
+  RECEIVE_FRIEND,
+  RECEIVE_FRIENDS,
+  REMOVE_FRIEND
+} from "../actions/friendActions"
+
+const friendsReducer = (oldState = {}, action) => {
+  Object.freeze(oldState)
+  let newState = Object.assign({}, oldState)
+
+  switch(action.type) {
+    case RECEIVE_FRIEND:
+      return Object.assign(newState, action.friend)
+    case RECEIVE_FRIENDS:
+      return action.friends
+    case REMOVE_FRIEND:
+      delete newState[action.id]
+      return newState
+    default:
+      return oldState
+  }
+}
+
+export default friendsReducer
