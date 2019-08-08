1e3b891ada10a46010ae02281fc85162a0e88660
Drew Rodrigues <thesimpledev@gmail.com>
2019-08-07 23:50:53 -0700

    Sort routes and fix trip routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
-  namespace :api, defaults: {format: :json} do
-    resources :users, only: [:create, :destroy, :update, :index]
-    resources :trips, except: [:new, :edit]
+  namespace :api, defaults: { format: :json } do
     resources :friend_requests, only: [:create, :index, :destroy]
     resources :friends, only: [:create, :index, :destroy]
     resource  :session, only: [:create, :destroy]
-    resources :trips, except: [:index, :new, :edit]
+    resources :trips, except: [:new, :edit]
+    resources :users, only: [:create, :destroy, :update, :index]
   end
 
   if Rails.env.test? || Rails.env.development?
