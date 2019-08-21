b3f09a3b9c2ea937de28a4c38715b52ada2e10ad
Drew Rodrigues <thesimpledev@gmail.com>
2019-08-21 15:55:27 -0700

    Annotate models

--- app/models/itinerary_item.rb
+++ app/models/itinerary_item.rb
@@ -1,3 +1,27 @@
+# == Schema Information
+#
+# Table name: itinerary_items
+#
+#  id          :bigint           not null, primary key
+#  title       :string           not null
+#  description :text
+#  start_date  :date
+#  end_date    :date
+#  start_time  :time
+#  end_time    :time
+#  position    :integer
+#  trip_id     :bigint
+#  location    :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+# Indexes
+#
+#  index_itinerary_items_on_trip_id  (trip_id)
+#
+
+# TODO: ensure foreign_key database constraint on trip_id
+
 class ItineraryItem < ApplicationRecord
   belongs_to :trip
 
