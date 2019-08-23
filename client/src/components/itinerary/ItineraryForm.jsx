3fbf7914f8aa36cecc1f3de9a3e80babb28530f6
Drew Rodrigues <thesimpledev@gmail.com>
2019-08-22 17:13:03 -0700

    Allow all fields on itinerary item and improve spacing

--- client/src/components/itinerary/ItineraryForm.jsx
+++ client/src/components/itinerary/ItineraryForm.jsx
@@ -40,25 +40,32 @@ class ItineraryForm extends React.Component {
     e.preventDefault()
     const formData = new FormData()
 
-    formData.append('itinerary_item[title]', this.state.title)
+    formData.append(`itinerary_item[title]`, this.state.title)
+    formData.append(`itinerary_item[description]`, this.state.description)
+    formData.append(`itinerary_item[position]`, this.state.position)
+
+    if (this.state.useDates) {
+      formData.append('itinerary_item[start_date]', this.state.date_range[0])
+      formData.append('itinerary_item[end_date]', this.state.date_range[1])
+    } else {
+      formData.append('itinerary_item[start_date]', null)
+      formData.append('itinerary_item[end_date]', null)
+    }
+
+    if (this.state.useTimes) {
+      formData.append('itinerary_item[start_time]', this.state.start_time)
+      formData.append('itinerary_item[end_time]', this.state.end_time)
+    } else {
+      formData.append('itinerary_item[start_time]', null)
+      formData.append('itinerary_item[end_time]', null)
+    }
+
     if (this.state.photo) {
       formData.append('itinerary_item[photo]', this.state.photo)
     }
-    // debugger
     for (let i = 0; i < this.state.files.length; i++) {
-      // debugger
       formData.append('itinerary_item[files][]', this.state.files[i])
     }
-
-
-    // const params = {
-    //   ...this.state,
-    //   start_date: (this.state.useDates ? this.state.date_range[0] : null),
-    //   end_date: (this.state.useDates ? this.state.date_range[1] : null),
-    //   start_time: (this.state.useTimes ? this.state.start_time : null),
-    //   end_time: (this.state.useTimes ? this.state.end_time : null),
-    //   photo: this.state.photo
-    // }
 
     this.props.action(formData)
       .then(() => {
